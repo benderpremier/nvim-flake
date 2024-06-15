dfaec69ba8a39e2678c8b0bb13ca5cd2193ae5b7
Damien Favre <dam.favre@gmail.com>
2024-06-15 22:25:54 +0200

    try fixing

--- nvim/plugin/nvim-metals.lua
+++ nvim/plugin/nvim-metals.lua
@@ -1,10 +1,17 @@
+local metals_config = require("metals").bare_config()
+
+-- Example of settings
+metals_config.settings = {
+  showImplicitArguments = true,
+  excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
+  metalsBinaryPath = vim.api.nvim_get_var('nix_dependant_configs').metals_path
+}
+
 local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "scala", "sbt", "java" },
   callback = function()
-    require("metals").initialize_or_attach({
-        metalsBinaryPath = vim.api.nvim_get_var('nix_dependant_configs').metals_path
-    })
+    require("metals").initialize_or_attach(metals_config)
   end,
   group = nvim_metals_group,
 })
