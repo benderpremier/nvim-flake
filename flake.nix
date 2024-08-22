2c53016879978096c04bc9262e6712758c62484a
souris <souris@benders-macbook-pro.home>
2024-08-22 22:18:27 +0200

    fix eyeliner

--- flake.nix
+++ flake.nix
@@ -12,6 +12,10 @@
     #   url = "github:Cassin01/wf.nvim";
     #   flake = false;
     # };
+     eyeliner = {
+       url = "github:jinh0/eyeliner.nvim";
+       flake = false;
+     };
   };
 
   outputs = inputs @ {
