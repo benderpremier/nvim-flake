de2c4643a30a0c06ff9748f8ec2c1c28819a3325
souris <souris@benders-macbook-pro.home>
2024-09-14 22:49:22 +0200

    dont close dap ui on exit

--- nvim/plugin/nvim-dap.lua
+++ nvim/plugin/nvim-dap.lua
@@ -29,12 +29,12 @@ end
 dap.listeners.before.launch.dapui_config = function()
   dapui.open()
 end
-dap.listeners.before.event_terminated.dapui_config = function()
-  dapui.close()
-end
-dap.listeners.before.event_exited.dapui_config = function()
-  dapui.close()
-end
+-- dap.listeners.before.event_terminated.dapui_config = function()
+--   dapui.close()
+-- end
+-- dap.listeners.before.event_exited.dapui_config = function()
+--   dapui.close()
+-- end
 
 vim.keymap.set('n', '<leader>dt', ":lua require('dapui').toggle()<CR>", { noremap = true,  desc = '[d]ap ui [t]oggle' })
 vim.keymap.set('n', '<leader>db', ":DapToggleBreakpoint<CR>", { noremap = true,  desc = '[d]ap toggle [b]reakpoint' })
