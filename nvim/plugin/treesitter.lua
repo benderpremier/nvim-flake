69a354958b1cf9de4f85b0114cc5ade74985d1ec
Damien Favre <dam.favre@gmail.com>
2026-02-08 13:36:26 +0100

    fix treesitter launch

--- nvim/plugin/treesitter.lua
+++ nvim/plugin/treesitter.lua
@@ -3,95 +3,118 @@ if vim.g.did_load_treesitter_plugin then
 end
 vim.g.did_load_treesitter_plugin = true
 
-local configs = require('nvim-treesitter.configs')
-vim.g.skip_ts_context_comment_string_module = true
+-- Defer loading until plugins are available
+local retry_count = 0
+local max_retries = 10
+local function setup_treesitter()
+  local ok, configs = pcall(require, 'nvim-treesitter.configs')
+  if not ok then
+    retry_count = retry_count + 1
+    if retry_count < max_retries then
+      vim.schedule(setup_treesitter)
+    else
+      vim.notify('nvim-treesitter.configs not found after ' .. max_retries .. ' retries', vim.log.levels.ERROR)
+    end
+    return
+  end
 
----@diagnostic disable-next-line: missing-fields
-configs.setup {
-  -- ensure_installed = 'all',
-  -- auto_install = false, -- Do not automatically install missing parsers when entering buffer
-  highlight = {
-    enable = true,
-    disable = function(_, buf)
-      local max_filesize = 100 * 1024 -- 100 KiB
-      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-      if ok and stats and stats.size > max_filesize then
-        return true
-      end
-    end,
-  },
-  textobjects = {
-    select = {
+  vim.g.skip_ts_context_comment_string_module = true
+
+  ---@diagnostic disable-next-line: missing-fields
+  configs.setup {
+    -- ensure_installed = 'all',
+    -- auto_install = false, -- Do not automatically install missing parsers when entering buffer
+    highlight = {
       enable = true,
-      -- Automatically jump forward to textobject, similar to targets.vim
-      lookahead = true,
-      keymaps = {
-        ['af'] = '@function.outer',
-        ['if'] = '@function.inner',
-        ['ac'] = '@class.outer',
-        ['ic'] = '@class.inner',
-        ['aC'] = '@call.outer',
-        ['iC'] = '@call.inner',
-        ['a#'] = '@comment.outer',
-        ['i#'] = '@comment.outer',
-        ['ai'] = '@conditional.outer',
-        ['ii'] = '@conditional.outer',
-        ['al'] = '@loop.outer',
-        ['il'] = '@loop.inner',
-        ['aP'] = '@parameter.outer',
-        ['iP'] = '@parameter.inner',
+      disable = function(_, buf)
+        local max_filesize = 100 * 1024 -- 100 KiB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+          return true
+        end
+      end,
+    },
+    textobjects = {
+      select = {
+        enable = true,
+        -- Automatically jump forward to textobject, similar to targets.vim
+        lookahead = true,
+        keymaps = {
+          ['af'] = '@function.outer',
+          ['if'] = '@function.inner',
+          ['ac'] = '@class.outer',
+          ['ic'] = '@class.inner',
+          ['aC'] = '@call.outer',
+          ['iC'] = '@call.inner',
+          ['a#'] = '@comment.outer',
+          ['i#'] = '@comment.outer',
+          ['ai'] = '@conditional.outer',
+          ['ii'] = '@conditional.outer',
+          ['al'] = '@loop.outer',
+          ['il'] = '@loop.inner',
+          ['aP'] = '@parameter.outer',
+          ['iP'] = '@parameter.inner',
+        },
+        selection_modes = {
+          ['@parameter.outer'] = 'v', -- charwise
+          ['@function.outer'] = 'V', -- linewise
+          ['@class.outer'] = '<c-v>', -- blockwise
+        },
       },
-      selection_modes = {
-        ['@parameter.outer'] = 'v', -- charwise
-        ['@function.outer'] = 'V', -- linewise
-        ['@class.outer'] = '<c-v>', -- blockwise
+      swap = {
+        enable = true,
+        swap_next = {
+          ['<leader>a'] = '@parameter.inner',
+        },
+        swap_previous = {
+          ['<leader>A'] = '@parameter.inner',
+        },
+      },
+      move = {
+        enable = true,
+        set_jumps = true, -- whether to set jumps in the jumplist
+        goto_next_start = {
+          [']m'] = '@function.outer',
+          [']P'] = '@parameter.outer',
+        },
+        goto_next_end = {
+          [']m'] = '@function.outer',
+          [']P'] = '@parameter.outer',
+        },
+        goto_previous_start = {
+          ['[m'] = '@function.outer',
+          ['[P'] = '@parameter.outer',
+        },
+        goto_previous_end = {
+          ['[m'] = '@function.outer',
+          ['[P'] = '@parameter.outer',
+        },
+      },
+      nsp_interop = {
+        enable = true,
+        peek_definition_code = {
+          ['df'] = '@function.outer',
+          ['dF'] = '@class.outer',
+        },
       },
     },
-    swap = {
-      enable = true,
-      swap_next = {
-        ['<leader>a'] = '@parameter.inner',
-      },
-      swap_previous = {
-        ['<leader>A'] = '@parameter.inner',
-      },
-    },
-    move = {
-      enable = true,
-      set_jumps = true, -- whether to set jumps in the jumplist
-      goto_next_start = {
-        [']m'] = '@function.outer',
-        [']P'] = '@parameter.outer',
-      },
-      goto_next_end = {
-        [']m'] = '@function.outer',
-        [']P'] = '@parameter.outer',
-      },
-      goto_previous_start = {
-        ['[m'] = '@function.outer',
-        ['[P'] = '@parameter.outer',
-      },
-      goto_previous_end = {
-        ['[m'] = '@function.outer',
-        ['[P'] = '@parameter.outer',
-      },
-    },
-    nsp_interop = {
-      enable = true,
-      peek_definition_code = {
-        ['df'] = '@function.outer',
-        ['dF'] = '@class.outer',
-      },
-    },
-  },
-}
+  }
 
-require('treesitter-context').setup {
-  max_lines = 3,
-}
+  local ok_context, _ = pcall(require, 'treesitter-context')
+  if ok_context then
+    require('treesitter-context').setup {
+      max_lines = 3,
+    }
+  end
 
-require('ts_context_commentstring').setup()
+  local ok_comment, _ = pcall(require, 'ts_context_commentstring')
+  if ok_comment then
+    require('ts_context_commentstring').setup()
+  end
 
--- Tree-sitter based folding
--- vim.opt.foldmethod = 'expr'
-vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+  -- Tree-sitter based folding
+  -- vim.opt.foldmethod = 'expr'
+  vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+end
+
+setup_treesitter()
